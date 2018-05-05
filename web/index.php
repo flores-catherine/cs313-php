d8e60fc9b7007c9c199b5a695cf0cddd5bb4dbfb
Catherine <catherine.g.flores@gmail.com>
2018-05-05 09:12:59 -0600

    moved files to see if it would work better not as the index or something....

--- web/index.php
+++ web/index.php
@@ -1,5 +1,19 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <title>Catherine's Page</title>
+    <meta name="description" content="Homepage For Catherine's CS 313">
+</head>    
+<body>
+    <h1>This is a homepage</h1>
+    <header>
+        <a>CS 313 Assignments</a>
+        <?php 
+        //print date
+        echo date("l jS \of F Y");
+        ?>
+    
+    </header>
+</body>    
+</html>
